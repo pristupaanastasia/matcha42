d8676692c331391f707e43b09b3243a01bc5d16a
mriley <pristupa1998@mail.ru>
2021-02-25 17:45:22 +0300

    add sql

---
+++ create.sql
@@ -0,0 +1,47 @@
+CREATE TABLE chat (
+        id_chat         SERIAL         NOT NULL,
+        user_one_id         int         NOT NULL,
+        user_two_id         int         NOT NULL
+        PRIMARY KEY (id_chat)
+);
+CREATE TABLE message (
+        id_chat       int  REFERENCES chat NOT NULL,
+        id_message    SERIAL         NOT NULL,
+        sender_id         int         NOT NULL,
+        receiver_id         int         NOT NULL,
+        message         char(250)   NOT NULL,
+        time         time           NOT NULL,
+        receiver_viewed int         NOT NULL
+        PRIMARY KEY (id_message)
+);
+
+CREATE TABLE user (
+        id_user         SERIAL         NOT NULL,
+        login         char(64)         NOT NULL,
+        password         char(80)         NOT NULL,
+        first_name   char(150)         NOT NULL,
+        last_name    char(150)         NOT NULL
+        PRIMARY KEY (id_user)
+);
+
+CREATE TABLE prifile (
+        id_user   int      REFERENCES user NOT NULL,
+        age         int,
+        image         char(150)[] ,
+        description   char(400) ,
+        sex    int NOT NULL,
+        tags char(50)[],
+        gps char(150) NOT NULL,
+        fame_rating int NOT NULL,
+        online int
+        PRIMARY KEY (id_user)
+);
+CREATE TABLE connect (
+        id_user   int      REFERENCES user NOT NULL,
+        like_user int[],
+        liked int[],
+        block int[],
+        blocked int[],
+        history int[]
+        PRIMARY KEY (id_user)
+);
